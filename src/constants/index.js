342d68d7fe0393a6c90cff4fd03372787640762c
M-TharunKumar <166986296+M-TharunKumar@users.noreply.github.com>
2024-11-28 11:42:50 +0530

    portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,62 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like HTML,CSS,Javascript and React, as well as back-end technologies like ,Python, MySQL,and Django. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React,Python, MySQL, and Django. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const INTENSHIP = [
+
+  {
+    year: "2024-present",
+    title: "Python Full Stack Developer",
+    description: "An intensive internship program focusing on developing end-to-end web applications using Python for backend development and modern frontend technologies.",
+    technologies: [
+      "Python","Django","HTML","CSS","JavaScript","SQL","Git",]
+  },
+  
+  {
+    year: "2023 - 2024",
+    title: "Frontend Developer",
+    description: `Designed and developed user interfaces for web applications using  React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "java sript", "mySQL","React"],
+  },
+  {
+    year: "2022 - 2023",
+    title: "Full Stack Developer",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Java Script","Django"],
+  },
+  {
+    title: "Wheather app",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A responsive application that provides real-time weather updates, including temperature, humidity, and forecasts for multiple locations, using APIs for accurate data retrieval",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  
+];
+
+export const CONTACT = {
+  address: "BTM Layout,22nd cross,Bangalore ,karanataka,560068",
+  phoneNo: " 6302693232",
+  email: "mtharunkumarnglp@gmail.com",
+};
